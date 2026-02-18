6a0072e6e83f57e5a56e83ad474058dc4e46ea80
Usaimanazir <usaimanazir123@gmail.com>
2026-02-19 00:32:13 +0530

    Add files via upload

---
+++ UC3_OOPSBannerapp.java
@@ -0,0 +1,13 @@
+public class UC3_OOPSBannerapp{
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" "," ****** ", " ****** ", " ******** ", " ****** "));
+        System.out.println(String.join(" ","**    **", "**    **", " **    ** ", "**    **"));
+        System.out.println(String.join(" ","**    **", "**    **", " **    ** ", "**      "));
+        System.out.println(String.join(" ","**    **", "**    **", " ******** ", " ****** "));
+        System.out.println(String.join(" ","**    **", "**    **", " **       ", "**    **"));
+        System.out.println(String.join(" ","**    **", "**    **", " **       ", "**    **"));
+        System.out.println(String.join(" "," ****** ", " ****** ", " **       ", " ****** "));
+    }
+}
+
